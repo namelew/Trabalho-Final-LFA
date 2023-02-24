98c575f81480f37224edf8f9cad4dc97e961058a
“namelew” <diogomaciel.cunha@gmail.com>
2023-02-24 11:39:07 -0300

    creating readTokens

--- input/file.go
+++ input/file.go
@@ -40,7 +40,9 @@ func rm (sl []string,s string) []string {
 	return sl
 }
 
-func readGR(rules []Rule, unames *[]string,entry string) []Rule{
+func readGR(unames *[]string,entry string) []Rule{
+	var rules []Rule
+
 	for _, line := range strings.Split(entry, "\n") {
 		if len(line) < 2{
 			continue
@@ -56,23 +58,10 @@ func readGR(rules []Rule, unames *[]string,entry string) []Rule{
 	return rules
 }
 
-func ReadRules(filename string) []Rule {
-	rules := make([]Rule, 0, 10)
+func readTokens(unames *[]string,entry string) []Rule{
+	var rules []Rule
 
-	data, err := os.ReadFile("rules.in")
-
-	unames := Names
-
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	temp := strings.Split(string(data), "--")
-	rtokens,gr := temp[0],temp[1] 
-
-	rules = readGR(rules, &unames, gr)
-
-	lines := strings.Split(rtokens,"\n")
-	var rw []Rule
+	lines := strings.Split(entry,"\n")
 
 	for _,line := range lines{
 		if line == ""{
@@ -81,7 +70,7 @@ func ReadRules(filename string) []Rule {
 		var afstate string
 		for id,r := range line {
 			created := func (s string) (bool, int, string) {
-				for _,rl := range rw {
+				for _,rl := range rules {
 					if s == string(rl.Productions[0][0]) {
 						return true,len(rl.Productions),rl.Productions[0][1:]
 					}
@@ -93,15 +82,15 @@ func ReadRules(filename string) []Rule {
 
 			if !yes && id < len(line) - 1{
 				if afstate == "" {
-					afstate = "<"+pop(&unames)+">"
-					rw = append(rw, Rule{"<"+pop(&unames)+">", []string{string(r)+afstate}})
+					afstate = "<"+pop(unames)+">"
+					rules = append(rules, Rule{"<"+pop(unames)+">", []string{string(r)+afstate}})
 				} else {
 					n := afstate
-					afstate = "<"+pop(&unames)+">"
+					afstate = "<"+pop(unames)+">"
 					if id == len(line) - 2 {
-						rw = append(rw, Rule{n, []string{string(r)+afstate, string(r)}})
+						rules = append(rules, Rule{n, []string{string(r)+afstate, string(r)}})
 					} else {
-						rw = append(rw, Rule{n, []string{string(r)+afstate}})
+						rules = append(rules, Rule{n, []string{string(r)+afstate}})
 					}
 				}
 			} else if yes && tam > 1 {
@@ -110,7 +99,23 @@ func ReadRules(filename string) []Rule {
 		}
 	}
 
-	fmt.Println(rw)
+	return rules
+}
+
+func ReadRules(filename string) []Rule {
+	data, err := os.ReadFile("rules.in")
+
+	unames := Names
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	temp := strings.Split(string(data), "--")
+	rtokens,gr := temp[0],temp[1] 
+
+	rules := readGR(&unames, gr)
+
+	fmt.Println(readTokens(&unames, rtokens))
 
 	Names = nil
 
